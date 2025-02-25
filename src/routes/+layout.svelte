f14c0b715a577d7126b8029db944dc60290286f5
Ye0l <casiya03@gmail.com>
2025-02-25 23:59:22 +0900

    init bootstrap

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import 'bootstrap/dist/css/bootstrap.min.css';
+  import { browser } from '$app/environment';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if(browser) {
+      import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  })
+</script>
+
+<slot />
